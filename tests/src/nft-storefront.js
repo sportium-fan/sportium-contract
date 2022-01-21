f3f1553983de85a765c3aeb4987da7d3e3c740d9
Dan <ryzond6728@gmail.com>
2022-01-21 18:57:42 +0900

    chore: change comment style for better IDE support

--- tests/src/nft-storefront.js
+++ tests/src/nft-storefront.js
@@ -9,11 +9,11 @@ import { setupFUSDOnAccount } from "./fusd";
 import { deployMoments, setupMomentsOnAccount } from "./moments";
 import { deployTreasury } from "./treasury";
 
-/*
+/**
  * Deploys Moments and NFTStorefront contracts to ElvnAdmin.
  * @throws Will throw an error if transaction is reverted.
  * @returns {Promise<*>}
- * */
+ */
 export const deployNFTStorefront = async () => {
 	const ElvnAdmin = await getElvnAdminAddress();
 
@@ -31,12 +31,12 @@ export const deployNFTStorefront = async () => {
 	return deployContractByNameWithErrorRaised({ to: ElvnAdmin, name: "NFTStorefront", addressMap });
 };
 
-/*
+/**
  * Sets up NFTStorefront.Storefront on account and exposes public capability.
  * @param {string} account - account address
  * @throws Will throw an error if transaction is reverted.
  * @returns {Promise<*>}
- * */
+ */
 export const setupStorefrontOnAccount = async (account) => {
 	// Account shall be able to Elvn
 	await setupElvnOnAccount(account);
@@ -51,14 +51,14 @@ export const setupStorefrontOnAccount = async (account) => {
 	return sendTransactionWithErrorRaised({ name, signers });
 };
 
-/*
+/**
  * Lists item with id equal to **item** id for sale with specified **price**.
  * @param {string} seller - seller account address
  * @param {UInt64} itemId - id of item to sell
  * @param {UFix64} price - price
  * @throws Will throw an error if transaction is reverted.
  * @returns {Promise<*>}
- * */
+ */
 export const createItemListing = async (seller, itemId, price) => {
 	const name = "nftStorefront/create_listing";
 	const args = [itemId, price];
@@ -67,14 +67,14 @@ export const createItemListing = async (seller, itemId, price) => {
 	return sendTransactionWithErrorRaised({ name, args, signers });
 };
 
-/*
+/**
  * Buys item with id equal to **item** id for **price** from **seller**.
  * @param {string} buyer - buyer account address
  * @param {UInt64} resourceId - resource uuid of item to sell
  * @param {string} seller - seller account address
  * @throws Will throw an error if transaction is reverted.
  * @returns {Promise<*>}
- * */
+ */
 export const purchaseItemListing = async (buyer, resourceId, seller) => {
 	const name = "nftStorefront/purchase_listing";
 	const args = [resourceId, seller];
@@ -91,13 +91,13 @@ export const purchaseItemListingPaymentByFUSD = async (buyer, resourceId, seller
 	return sendTransactionWithErrorRaised({ name, args, signers });
 };
 
-/*
+/**
  * Removes item with id equal to **item** from sale.
  * @param {string} owner - owner address
  * @param {UInt64} itemId - id of item to remove
  * @throws Will throw an error if transaction is reverted.
  * @returns {Promise<*>}
- * */
+ */
 export const removeItemListing = async (owner, itemId) => {
 	const name = "nftStorefront/remove_listing";
 	const signers = [owner];
@@ -106,12 +106,12 @@ export const removeItemListing = async (owner, itemId) => {
 	return sendTransactionWithErrorRaised({ name, args, signers });
 };
 
-/*
+/**
  * Returns the number of items for sale in a given account's storefront.
  * @param {string} account - account address
  * @throws Will throw an error if execution will be halted
  * @returns {UInt64}
- * */
+ */
 export const getListingCount = async (account) => {
 	const name = "nftStorefront/get_listings_length";
 	const args = [account];
