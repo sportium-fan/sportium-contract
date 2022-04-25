61ecd7df3fb859d2e24482bfc82986d5f0121b78
Jiseok CHOI <jiseok.dev@gmail.com>
2022-04-25 15:56:11 +0900

    Bump version to 0.20.0

--- dist/transactions/pack/destroyStorage.js
+++ dist/transactions/pack/destroyStorage.js
@@ -6,11 +6,11 @@ import Pack from 0xPack
 
 transaction {
     prepare(account: AuthAccount) {
-		  let storefront <- account
-        .load<@Pack.Token>(from: Pack.CollectionStoragePath)
-        ?? panic("Not Found Pack.Storefront Resource")
+		  let pack <- account
+        .load<@Pack.Collection>(from: Pack.CollectionStoragePath)
+        ?? panic("Not Found Pack.Collection Resource")
 
-      destroy storefront
+      destroy pack 
     }
 }
 `;
